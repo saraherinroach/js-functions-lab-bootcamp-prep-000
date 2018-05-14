2c838ce7e8ca73d79daa61ddfc53c6aa7a36314f
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-14 21:31:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // write your code below!
-function happyHolidays() {
+function happyHolidays() { return "Happy holidays!"}
+function happyHolidaysTo (name) {return `Happy holidays, ${name}!`}
+function happyHolidayTo (holiday, name) {return `Happy ${holiday}, ${name}!`}
+function holidayCountdown (days, holiday) {return `It's ${days} days until ${holiday}!`}
